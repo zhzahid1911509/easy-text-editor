36133d189f0d8454e728c64528facadfe08b4960
Zahid Hasan <67813593+zhzahid1911509@users.noreply.github.com>
2023-08-17 22:25:24 +0600

    overall updated

--- scripts/font_size.js
+++ scripts/font_size.js
@@ -2,7 +2,7 @@ const fontInput = getValueOfId("font-size");
 
 document.getElementById("font-size").addEventListener("input", function(){
     if(fontInput.value == ""){
-        textarea.style.fontSize = '10px';
+        textarea.style.fontSize = '12pt';
     }  
-    textarea.style.fontSize = fontInput.value + 'px';
+    textarea.style.fontSize = fontInput.value + 'pt';
 });
