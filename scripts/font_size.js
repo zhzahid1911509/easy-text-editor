ec427cfa255f2368fa6b534efba85c82bbfa86f3
Zahid Hasan <67813593+zhzahid1911509@users.noreply.github.com>
2023-08-17 19:57:10 +0600

    font size updated

--- scripts/font_size.js
+++ scripts/font_size.js
@@ -1,4 +1,8 @@
+const fontInput = getValueOfId("font-size");
+
 document.getElementById("font-size").addEventListener("input", function(){
-    const fontInput = getValueOfId("font-size");
-    textarea.style.fontSize = fontInput.value + 'px';  
+    if(fontInput.value == ""){
+        textarea.style.fontSize = '16px';
+    }  
+    textarea.style.fontSize = fontInput.value + 'px';
 });
